891b45c1abad38cf9cacca0151e5c44439f11612
hemalgetweb <hemalgetweb@gmail.com>
2023-09-08 23:38:03 +0600

    test

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,95 @@
-import chalk from 'chalk';
-import { commandDetails, commandHistory, commands } from './utility/commands';
-import { input, rl } from './utility/input';
+// import chalk from 'chalk';
+// import { commandDetails, commandHistory, commands } from './utility/commands';
+// import { input, rl } from './utility/input';
 
-console.clear();
+// console.clear();
 
-const runCommand = async (
-    command: keyof typeof commandDetails,
-    args?: string[],
-) => {
-    const commandDetail = commandDetails[command];
+// const runCommand = async (
+//     command: keyof typeof commandDetails,
+//     args?: string[],
+// ) => {
+//     const commandDetail = commandDetails[command];
 
-    if (!commandDetail.action) {
-        return;
-    }
+//     if (!commandDetail.action) {
+//         return;
+//     }
 
-    await commandDetail.action(args);
-};
+//     await commandDetail.action(args);
+// };
 
-const handler =
-    '┌ ' +
-    chalk.greenBright('@Visitor') +
-    ' ➜  ' +
-    chalk.blueBright('me.toinfinite.dev (') +
-    chalk.redBright('master') +
-    chalk.blueBright(')') +
-    '\n' +
-    '└ $ ';
+// const handler =
+//     '┌ ' +
+//     chalk.greenBright('@Visitor') +
+//     ' ➜  ' +
+//     chalk.blueBright('me.toinfinite.dev (') +
+//     chalk.redBright('master') +
+//     chalk.blueBright(')') +
+//     '\n' +
+//     '└ $ ';
 
-runCommand('welcome');
+// runCommand('welcome');
 
-const processCommand = async (commandLine: string): Promise<void> => {
-    const commandParts = commandLine?.match(/\b\w+\b/g);
-    const commandName = commandParts?.[0];
-    const commandArgs = commandParts?.slice(1);
+// const processCommand = async (commandLine: string): Promise<void> => {
+//     const commandParts = commandLine?.match(/\b\w+\b/g);
+//     const commandName = commandParts?.[0];
+//     const commandArgs = commandParts?.slice(1);
 
-    if (commandName === undefined || commandName?.trim() === '') {
-        return await askQuestion();
-    } else if (commandName === 'exit') {
-        console.log(chalk.redBright('\nHasta la vista.\n'));
-        return rl.close();
-    }
+//     if (commandName === undefined || commandName?.trim() === '') {
+//         return await askQuestion();
+//     } else if (commandName === 'exit') {
+//         console.log(chalk.redBright('\nHasta la vista.\n'));
+//         return rl.close();
+//     }
 
-    if (!commandDetails[commandName as keyof typeof commandDetails]) {
-        console.log(
-            chalk.redBright(
-                `\nCommand ${chalk.whiteBright(
-                    commandName,
-                )} not found. Try ${chalk.whiteBright('help')}.\n`,
-            ),
-        );
-        return await askQuestion();
-    }
+//     if (!commandDetails[commandName as keyof typeof commandDetails]) {
+//         console.log(
+//             chalk.redBright(
+//                 `\nCommand ${chalk.whiteBright(
+//                     commandName,
+//                 )} not found. Try ${chalk.whiteBright('help')}.\n`,
+//             ),
+//         );
+//         return await askQuestion();
+//     }
 
-    await runCommand(commandName as keyof typeof commandDetails, commandArgs);
+//     await runCommand(commandName as keyof typeof commandDetails, commandArgs);
 
-    // save command history
-    commandHistory.push(commandLine);
-    if (commandHistory.length > 50) {
-        commandHistory.shift();
-    }
+//     // save command history
+//     commandHistory.push(commandLine);
+//     if (commandHistory.length > 50) {
+//         commandHistory.shift();
+//     }
 
-    console.log();
-    askQuestion();
-};
+//     console.log();
+//     askQuestion();
+// };
 
-const askQuestion = () => {
-    input(handler, (answer) => {
-        processCommand(answer);
-    });
-};
+// const askQuestion = () => {
+//     input(handler, (answer) => {
+//         processCommand(answer);
+//     });
+// };
 
-// show suggestions
-process.stdin.on('keypress', (c, k) => {
-    if (k.name === 'tab') {
-        // if (k.sequence === '\t') {
-        //     return;
-        // }
+// // show suggestions
+// process.stdin.on('keypress', (c, k) => {
+//     if (k.name === 'tab') {
+//         // if (k.sequence === '\t') {
+//         //     return;
+//         // }
 
-        console.log(`\n\nSuggestions:`);
-        const suggestions = [
-            ...commands.filter((command) => command.startsWith(rl.line.trim())),
-        ].join('  ');
+//         console.log(`\n\nSuggestions:`);
+//         const suggestions = [
+//             ...commands.filter((command) => command.startsWith(rl.line.trim())),
+//         ].join('  ');
 
-        console.log(suggestions + '\n');
+//         console.log(suggestions + '\n');
 
-        process.stdout.write(handler);
-        rl.write(rl.line.trim());
-        // return askQuestion();
-    }
-});
+//         process.stdout.write(handler);
+//         rl.write(rl.line.trim());
+//         // return askQuestion();
+//     }
+// });
 
-askQuestion();
+// askQuestion();
+
+console.log('Hello World');
